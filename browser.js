a10b31fc58ad7951d58cf5e0a5a1a9e7bfbaf455
accraze <accraze@gmail.com>
2014-12-19 09:30:23 -0700

    popup on hover

--- browser.js
+++ browser.js
@@ -6,7 +6,7 @@ require('leaflet-providers');
 L.Icon.Default.imagePath = '/leaflet/images';
 
 var map = L.map('map');
-map.setView([33.4294, -111.9431],11); //phoenix metro area
+map.setView([33.4294, -111.9431],9); //phoenix metro area
 var layer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/spatial.b625e395/{z}/{x}/{y}.png');
 layer.addTo(map);
 
@@ -14,14 +14,48 @@ var drawnItems, drawControl;
 
 request('/geoms', function (err, res) {
 	var existingGeoms = JSON.parse(res.body);
-	drawnItems = L.geoJson(existingGeoms);
-	map.addLayer(drawnItems);
+	//drawnItems = L.geoJson(existingGeoms);
+	//map.addLayer(drawnItems);
 
-	drawControl = new L.Control.Draw({
-		edit: {featureGroup: drawnItems}
-	});
+    var runs = L.geoJson(existingGeoms, {
+        filter: function(feature, layer) {
+            return feature.properties.BusType == "Run";
+        },
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+                //icon: cafeIcon
+            }).on('mouseover', function() {
+                this.bindPopup(feature.properties.Name).openPopup();
+            });
+        }
+    });
 
-	map.addControl(drawControl);
+
+    var hikes = L.geoJson(existingGeoms, {
+        filter: function(feature, layer) {
+            return feature.properties.ActType != "Run";
+        },
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+
+            }).on('mouseover', function() {
+                this.bindPopup(feature.properties.Name).openPopup();
+            });
+        }
+    });
+
+    // map.fitBounds(drawnItems.getBounds(), {
+    //     padding: [50, 50]
+    // });
+
+
+    runs.addTo(map)
+    hikes.addTo(map)
+
+    // drawControl = new L.Control.Draw({
+    //     edit: {featureGroup: drawnItems}
+    // });
+	//map.addControl(drawControl);
 });
 
 map.on('draw:created', function (e) {
@@ -32,48 +66,3 @@ map.on('draw:created', function (e) {
 		json: e.layer.toGeoJSON()
 	});
 });
-
-
-// var promise = $.getJSON("/data/hikes.json");
-//     promise.then(function(data) {
-
-
-//         var allactivities = L.geoJson(data);
-
-
-//         var runs = L.geoJson(data, {
-//             filter: function(feature, layer) {
-//                 return feature.properties.BusType == "Run";
-//             },
-//             pointToLayer: function(feature, latlng) {
-//                 return L.marker(latlng, {
-//                     icon: runIcon
-//                 }).on('mouseover', function() {
-//                     this.bindPopup(feature.properties.Name).openPopup();
-//                 });
-//             }
-//         });
-
-
-//         var others = L.geoJson(data, {
-//             filter: function(feature, layer) {
-//                 return feature.properties.BusType != "Run";
-//             },
-//             pointToLayer: function(feature, latlng) {
-//                 return L.marker(latlng, {
-
-//                 }).on('mouseover', function() {
-//                     this.bindPopup(feature.properties.Name).openPopup();
-//                 });
-//             }
-//         });
-
-//         map.fitBounds(allactivities.getBounds(), {
-//             padding: [50, 50]
-//         });
-
-
-//         runs.addTo(map)
-//         others.addTo(map)
-
-//   });
--- public/bundle.js
+++ public/bundle.js
@@ -7,7 +7,7 @@ require('leaflet-providers');
 L.Icon.Default.imagePath = '/leaflet/images';
 
 var map = L.map('map');
-map.setView([33.4294, -111.9431],11); //phoenix metro area
+map.setView([33.4294, -111.9431],9); //phoenix metro area
 var layer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/spatial.b625e395/{z}/{x}/{y}.png');
 layer.addTo(map);
 
@@ -15,14 +15,48 @@ var drawnItems, drawControl;
 
 request('/geoms', function (err, res) {
 	var existingGeoms = JSON.parse(res.body);
-	drawnItems = L.geoJson(existingGeoms);
-	map.addLayer(drawnItems);
+	//drawnItems = L.geoJson(existingGeoms);
+	//map.addLayer(drawnItems);
 
-	drawControl = new L.Control.Draw({
-		edit: {featureGroup: drawnItems}
-	});
+    var runs = L.geoJson(existingGeoms, {
+        filter: function(feature, layer) {
+            return feature.properties.BusType == "Run";
+        },
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+                //icon: cafeIcon
+            }).on('mouseover', function() {
+                this.bindPopup(feature.properties.Name).openPopup();
+            });
+        }
+    });
 
-	map.addControl(drawControl);
+
+    var hikes = L.geoJson(existingGeoms, {
+        filter: function(feature, layer) {
+            return feature.properties.ActType != "Run";
+        },
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+
+            }).on('mouseover', function() {
+                this.bindPopup(feature.properties.Name).openPopup();
+            });
+        }
+    });
+
+    // map.fitBounds(drawnItems.getBounds(), {
+    //     padding: [50, 50]
+    // });
+
+
+    runs.addTo(map)
+    hikes.addTo(map)
+
+    // drawControl = new L.Control.Draw({
+    //     edit: {featureGroup: drawnItems}
+    // });
+	//map.addControl(drawControl);
 });
 
 map.on('draw:created', function (e) {
@@ -34,50 +68,6 @@ map.on('draw:created', function (e) {
 	});
 });
 
-
-// var promise = $.getJSON("/data/hikes.json");
-//     promise.then(function(data) {
-
-
-//         var allactivities = L.geoJson(data);
-
-
-//         var runs = L.geoJson(data, {
-//             filter: function(feature, layer) {
-//                 return feature.properties.BusType == "Run";
-//             },
-//             pointToLayer: function(feature, latlng) {
-//                 return L.marker(latlng, {
-//                     icon: runIcon
-//                 }).on('mouseover', function() {
-//                     this.bindPopup(feature.properties.Name).openPopup();
-//                 });
-//             }
-//         });
-
-
-//         var others = L.geoJson(data, {
-//             filter: function(feature, layer) {
-//                 return feature.properties.BusType != "Run";
-//             },
-//             pointToLayer: function(feature, latlng) {
-//                 return L.marker(latlng, {
-
-//                 }).on('mouseover', function() {
-//                     this.bindPopup(feature.properties.Name).openPopup();
-//                 });
-//             }
-//         });
-
-//         map.fitBounds(allactivities.getBounds(), {
-//             padding: [50, 50]
-//         });
-
-
-//         runs.addTo(map)
-//         others.addTo(map)
-
-//   });
 },{"leaflet":"/Users/accraze/Sites/leaflet-draw-server/node_modules/leaflet/dist/leaflet-src.js","leaflet-draw":"/Users/accraze/Sites/leaflet-draw-server/node_modules/leaflet-draw/dist/leaflet.draw.js","leaflet-providers":"/Users/accraze/Sites/leaflet-draw-server/node_modules/leaflet-providers/leaflet-providers.js","xhr":"/Users/accraze/Sites/leaflet-draw-server/node_modules/xhr/index.js"}],"/Users/accraze/Sites/leaflet-draw-server/node_modules/leaflet-draw/dist/leaflet.draw.js":[function(require,module,exports){
 /*
 	Leaflet.draw, a plugin that adds drawing and editing tools to Leaflet powered maps.
